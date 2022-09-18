bfd8c78a4af5f605963a536818db181162c0e47d
Ale <tiscorniaale@gmail.com>
2022-09-18 20:46:40 -0300

    BaSP-W05-2022:exercise 2 fixed

--- Semana-05/exercices/02_strings.js
+++ Semana-05/exercices/02_strings.js
@@ -1,48 +1,40 @@
 // Strings
 // A) Crear una variable de tipo string con al menos 10 caracteres y convertir todo el 
 // texto en mayúscula (utilizar toUpperCase).
-// B) Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con 
-// los primeros 5 caracteres guardando el resultado en una nueva variable (utilizar substring).
-// C) Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con 
-// los últimos 3 caracteres guardando el resultado en una nueva variable (utilizar substring).
-// D) Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con la 
-// primea y las demás en minúscula. Guardar el resultado en una nueva
-// variable (utilizar substring, toUpperCase, toLowerCase y el operador +).
-// E) Crear una variable de tipo string con al menos 10 caracteres y algún espacio en blanco. 
-// Encontrar la posición del primer espacio en blanco y guardarla en una variable (utilizar indexOf).
-// F) Crear una variable de tipo string con al menos 2 palabras largas (10 caracteres y algún espacio entre medio). 
-// Utilizar los métodos de los ejercicios anteriores para generar un nuevo string que tenga la primera letra de 
-// ambas palabras en mayúscula y las demás letras en minúscula (utilizar indexOf, substring, toUpperCase, 
-// toLowerCase y el operador +).
-
-
-// A)
 var a = 'holamundoo';
 console.log(a.toUpperCase());
 
-// B)
+// B) Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con 
+// los primeros 5 caracteres guardando el resultado en una nueva variable (utilizar substring).
 var b = 'holamundoo';
 var c = b.substring (0,5);
 console.log(c);
 
-// C)
+// C) Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con 
+// los últimos 3 caracteres guardando el resultado en una nueva variable (utilizar substring).
 var d = 'holamundoo';
 var e = d.substring(7,10);
 console.log (e);
 console.log(l);
 
-// D)
+// D) Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con la 
+// primea y las demás en minúscula. Guardar el resultado en una nueva
+// variable (utilizar substring, toUpperCase, toLowerCase y el operador +).
 var f = 'holamundoo';
 var g = f.substring(0,1).toUpperCase() + f.substring(1,10);
 console.log(g)
 
-// E)
+// E) Crear una variable de tipo string con al menos 10 caracteres y algún espacio en blanco. 
+// Encontrar la posición del primer espacio en blanco y guardarla en una variable (utilizar indexOf).
 var h = 'hola mundo';
 var i = h.indexOf(' ');
 console.log(i);
 
-// F)
-var j = 'hola mundoo'; 
+// F) Crear una variable de tipo string con al menos 2 palabras largas (10 caracteres y algún espacio entre medio). 
+// Utilizar los métodos de los ejercicios anteriores para generar un nuevo string que tenga la primera letra de 
+// ambas palabras en mayúscula y las demás letras en minúscula (utilizar indexOf, substring, toUpperCase, 
+// toLowerCase y el operador +).
+var j = 'hOLa muNDOo'; 
 var k = j.indexOf(' ');
-var l = f.substring(0,1).toUpperCase() + f.substring(1,k) + f.substring(k,k+1).toUpperCase() + f.substring(k+1,j.length);
+var l = f.substring(0,1).toUpperCase() + f.substring(1,k).toLowerCase() + f.substring(k,k+1).toUpperCase() + f.substring(k+1,j.length).toLowerCase();
 console.log(l);
